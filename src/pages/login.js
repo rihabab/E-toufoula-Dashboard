421c567b08c87c70ee7194cb7f3ff315f61f4eb7
RihabAitBahessou <117985681+rihabab@users.noreply.github.com>
2024-02-16 00:35:13 +0100

    commit

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,7 +3,7 @@ import login from '../assets/onde.png'
 import { useState, useContext } from 'react';
 import Axios from "axios";
 import { MyContext } from '../providers/userContextProvider';
-
+import kidland from '../assets/kidland2.png'
 
 function Login(){
   const [email, setEmail]= useState('');
@@ -34,22 +34,93 @@ function Login(){
   };
 
     return(
-        <section className="h-screen">
-  <div className="h-full">
+//         <section className="h-screen">
+//   <div className="h-full">
     
-    <div
-      className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
-      <div
-        className="flex items-center justify-center shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
-        <img
-          src={login}
-          className=" w-2/3"
-           />
-      </div>
+//     <div
+//       className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
+//       <div
+//         className="flex items-center justify-center shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
+//         <img
+//           src={login}
+//           className=" w-2/3"
+//            />
+//       </div>
 
       
-      <div className="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12">
-        <form>
+//       <div className="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12">
+//         <form className='w-2/3'>
+          
+//           <div
+//             className="flex flex-row items-center justify-center lg:justify-start">
+//             <p className="mb-0 mr-4 text-lg">Sign in </p>
+
+            
+            
+            
+//           </div>
+
+          
+//           <div
+//             className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
+//             {/* <p
+//               className="mx-4 mb-0 text-center font-semibold dark:text-white">
+//               Or
+//             </p> */}
+//           </div>
+
+//          <div>{error}</div>
+          
+//           <div className="relative h-11 w-full min-w-[200px] my-5">
+//             <input placeholder="Email" type="email"
+//               className="peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50 placeholder:opacity-0 focus:placeholder:opacity-100" 
+//               onChange={(event) => {
+//                 setEmail(event.target.value);
+//               }}/>
+//             <label
+//               className="after:content[''] pointer-events-none absolute left-0  -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-500 after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-blue-gray-500 peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+//               Email address
+//             </label>
+//           </div>
+//           <div className="relative h-11 w-full min-w-[200px] my-5">
+//             <input placeholder="Password" type="password"
+//               className="peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50 placeholder:opacity-0 focus:placeholder:opacity-100" 
+//               onChange={(event) => {
+//                 setPassword(event.target.value);
+//               }}/>
+//             <label
+//               className="after:content[''] pointer-events-none absolute left-0  -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-500 after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-blue-gray-500 peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+//               Password
+//             </label>
+//           </div>
+          
+
+//           <div className="text-center lg:text-left">
+//             <button
+//               type="button"
+//               className="inline-block rounded bg-primary px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-black shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-slate-800 hover:text-white hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]  "
+//               onClick={handleButtonClick}>
+//               Login
+//             </button>
+
+//           </div>
+//         </form>
+//       </div>
+//     </div>
+//   </div>
+// </section>
+<div className="flex h-screen">
+      {/* Left side with image */}
+      <div className="w-1/2 bg-gray-200 flex items-center justify-center">
+        {/* Replace 'image-url' with your image URL */}
+        <img src={kidland} alt="Theme Image" className="object-cover w-full h-full" />
+
+      </div>
+
+      {/* Right side with sign-in form */}
+      <div className="w-1/2 bg-white flex items-center justify-center">
+        <div className="max-w-md w-full p-8 space-y-4">
+        <form className=''>
           
           <div
             className="flex flex-row items-center justify-center lg:justify-start">
@@ -95,26 +166,19 @@ function Login(){
           </div>
           
 
-          
-          
-
-          
-
           <div className="text-center lg:text-left">
             <button
               type="button"
-              className="inline-block rounded bg-primary px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-black shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+              className="inline-block rounded bg-primary px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-black shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-slate-800 hover:text-white hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]  "
               onClick={handleButtonClick}>
               Login
             </button>
 
           </div>
         </form>
+        </div>
       </div>
     </div>
-  </div>
-</section>
-
 
 
       
